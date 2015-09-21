17d57d2af5b2bfad11027866c8c58606d543f503
Eric Dubé <eric.alex.dube@gmail.com>
2015-09-21 12:50:26 -0400

    Update Lab1-Step-36.php

--- Lab1-Step-36.php
+++ Lab1-Step-36.php
@@ -1,51 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<style>
-		.label {
-			display: inline-block;
-			min-width: 100pt;
-		}
-	</style>
-</head>
-<body>
-<hr color="darkblue" size="20">
-<div style="-webkit-transform: rotateZ(160deg)">
-<h1 align="center" style="color:#F0F;">Welcome to UOIT</h1>
-</div>
-<hr color="darkblue" size="20">
-</body>
-</html>
 <?php
-$xyz = new DateTime("now", new DateTimeZone('America/Toronto'));
-echo $xyz->format('m/d/Y, H:i:s');
-?>
+$a1 = $_POST['a'];
+$b1 = $_POST['b'];
+$c1 = $_POST['c'];
+$d1 = $_POST['d'];
+$e1 = $_POST['e'];
 
-<hr color="darkblue" size="4">
-<form action="datastore.php" method="post">
-	<p>
-		<label class="label" for="strFirstName">First Name: </label>
-		<input  type="text" name="a" id="strFirstName" />
-	</p>
-	<p>
-		<label class="label" for="strLastName">Last Name: </label>
-		<input  type="text" name="b" id="strLastName" />
-	</p>
-	<p>
-		<label class="label" for="strUsername">Username: </label>
-		<input  type="text" name="c" id="strUsername" />
-	</p>
-	<p>
-		<label class="label" for="strPassword">Password: </label>
-		<input  type="text" name="d" id="strPassword" />
-	</p>
-	<p>
-		<label class="label" for="strCellphone">Cellphone #: </label>
-		<input  type="text" name="e" id="strCellphone" />
-	</p>
-	<p>
-		<label class="label">&nbsp;
-		</label>
-		<input type="submit" value="Register" />
-	</p>
-</form>
+?>
+<p>Greetings <?php echo $a1.' '.$b1; ?></p>
+<p>Your username is: <?php echo $c1; ?> and your password is: <?php echo $d1; ?></p>
+<p>Your cellphone number is <?php echo $e1; ?></p>
+<p><hr color="darkblue" size="4">End of Part3</p>
